c046347881ba4bdf08ff8ab91e7f904259bc648e
milagros <miesagdel@gmail.com>
2026-07-05 02:23:19 -0400

    agregando contenido

--- script.js
+++ script.js
@@ -29,3 +29,27 @@ if(total <= 10){
 }
 
 });
+
+const boton = document.getElementById("ingresar");
+
+boton.addEventListener("click", function(){
+
+let digito1 = document.getElementById("digito1").value;
+let digito2 = document.getElementById("digito2").value;
+let digito3 = document.getElementById("digito3").value;
+
+let password = digito1 + digito2 + digito3;
+
+const resultado = document.getElementById("resultado");
+
+if(password === "911"){
+    resultado.innerHTML = "Password 1 correcto";
+}
+else if(password === "714"){
+    resultado.innerHTML = "Password 2 correcto";
+}
+else{
+    resultado.innerHTML = "Password incorrecto";
+}
+
+});
