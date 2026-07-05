5f5c8eced91a29b1f2e747e23ef3ff018c67d992
milagros <miesagdel@gmail.com>
2026-07-05 02:22:02 -0400

    segundo ejercicio realizado

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ imagen.addEventListener("click", function(){
     }
 
 });
+
+const boton = document.getElementById("verificar");
+
+boton.addEventListener("click", function(){
+
+let sticker1 = Number(document.getElementById("sticker1").value);
+let sticker2 = Number(document.getElementById("sticker2").value);
+let sticker3 = Number(document.getElementById("sticker3").value);
+
+let total = sticker1 + sticker2 + sticker3;
+
+const resultado = document.getElementById("resultado");
+
+if(total <= 10){
+    resultado.innerHTML = "Llevas " + total + " stickers";
+}else{
+    resultado.innerHTML = "Llevas demasiados stickers";
+}
+
+});
